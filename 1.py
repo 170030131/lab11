293cb84b074181ced4ab1041bcfa1faa456ba826
170030131 <53849338+170030131@users.noreply.github.com>
2019-12-28 14:56:20 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -7,7 +7,7 @@ num = 11
 if num > 1: 
 	
 # Iterate from 2 to n / 2 
-for i in range(2, num//2): 
+for i in range(2, num/2): 
 		
 	# If num is divisible by any number between 
 	# 2 and n / 2, it is not prime 
